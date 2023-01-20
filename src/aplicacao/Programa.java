3b4fc5bc6d8bd8cdd0e9a13525016845cea94e1f
Lucasdaniel314 <lucasdanielmonteirodesousa827@gmail.com>
2023-01-20 19:52:46 -0300

    tem verificadr de bem estar e gitignore

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,12 +1,20 @@
 package aplicacao;
 
+import java.util.Scanner;
+
 public class Programa {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int x = 5;
-		int y = 7;
-		System.out.println(x * y);
+		Scanner sc = new Scanner(System.in);
+		System.out.print("como vai seu dia? ");
+		String estar = sc.nextLine();
+		if (estar == "bem"){
+			System.out.println("tenha um bom dia!");
+		} else if(estar == "mal"){
+			System.out.println("que pena");
+		}
+		sc.close();
 	}
 
 }

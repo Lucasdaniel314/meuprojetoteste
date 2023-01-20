8ba5ffe2c635746ebf2d878f055519da8e02c6e1
Lucasdaniel314 <lucasdanielmonteirodesousa827@gmail.com>
2023-01-20 11:48:28 -0300

    somador 5 mais 7

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -4,8 +4,9 @@ public class Programa {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("olá mundo");
-		//primeiro programa
+		int x = 5;
+		int y = 7;
+		System.out.println(x + y);
 	}
 
 }

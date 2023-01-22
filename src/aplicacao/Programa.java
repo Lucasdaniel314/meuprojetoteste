c9998bf17d579997370af08e41bd5ae85f4f2f20
Lucasdaniel314 <lucasdanielmonteirodesousa827@gmail.com>
2023-01-22 13:30:05 -0300

    comparador de ano bissexto

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -3,17 +3,16 @@ package aplicacao;
 import java.util.Scanner;
 
 public class Programa {
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//tabuada
+		//comparador de ano bissexto
 		Scanner sc = new Scanner(System.in);
-		int resultado = 0;
-		System.out.print("você que ver a tabuada de qual número? ");
-		int num = sc.nextInt();
-		for(int i = 0; i<=10; i++){
-			resultado = num * i;
-			System.out.println(num +" x "+ i + "= "+ resultado);
+		System.out.print("digite um ano: ");
+		int ano = sc.nextInt();
+		if (ano % 4 == 0 && (ano % 100 != 0 || ano % 400 == 0)){
+			System.out.println("o ano é bisexto");
+		} else {
+			System.out.println("o ano não é bisexto");
 		}
 		sc.close();
 	}
